c98884a2160a68f9479b3fb3c06d466d72550965
AlexFerras <78160840+AlexFerras@users.noreply.github.com>
2023-02-05 00:08:37 +0100

    Create StoreUIDComponent.cs

---
+++ Content.Server/Wedding/StoreUIDComponent.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Server.Wedding
+{
+    internal sealed class StoreUIDComponent
+    {
+        public EntityUid UID;
+    }
+}
