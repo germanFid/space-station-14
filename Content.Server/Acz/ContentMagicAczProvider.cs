e132e927d68965411eefdb1addd9d1d91136d878
Keidran <64165043+Keidran@users.noreply.github.com>
2023-08-08 21:02:56 +0300

    refact Acz

--- Content.Server/Acz/ContentMagicAczProvider.cs
+++ Content.Server/Acz/ContentMagicAczProvider.cs
@@ -1,9 +1,9 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
 using Content.Packaging;
 using Robust.Packaging;
 using Robust.Packaging.AssetProcessing;
 using Robust.Server.ServerStatus;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Content.Server.Acz;
 
