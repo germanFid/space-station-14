095fa5935fc7a0ed0f2c447e1a8b81a850c2231f
Keidran <64165043+Keidran@users.noreply.github.com>
2023-08-10 13:54:30 +0300

    refact Afk

--- Content.Server/Afk/IsAfkCommand.cs
+++ Content.Server/Afk/IsAfkCommand.cs
@@ -1,48 +1,47 @@
-﻿using Content.Server.Administration;
+using Content.Server.Administration;
 using Content.Shared.Administration;
 using Robust.Server.Player;
 using Robust.Shared.Console;
 
-namespace Content.Server.Afk
+namespace Content.Server.Afk;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class IsAfkCommand : IConsoleCommand
 {
-    [AdminCommand(AdminFlags.Admin)]
-    public sealed class IsAfkCommand : IConsoleCommand
+    [Dependency] private readonly IPlayerManager _players = default!;
+
+    public string Command => "isafk";
+    public string Description => "Checks if a specified player is AFK";
+    public string Help => "Usage: isafk <playerName>";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IPlayerManager _players = default!;
+        var afkManager = IoCManager.Resolve<IAfkManager>();
 
-        public string Command => "isafk";
-        public string Description => "Checks if a specified player is AFK";
-        public string Help => "Usage: isafk <playerName>";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (args.Length == 0)
         {
-            var afkManager = IoCManager.Resolve<IAfkManager>();
-
-            if (args.Length == 0)
-            {
-                shell.WriteError("Need one argument");
-                return;
-            }
-
-            if (!_players.TryGetSessionByUsername(args[0], out var player))
-            {
-                shell.WriteError("Unable to find that player");
-                return;
-            }
-
-            shell.WriteLine(afkManager.IsAfk(player) ? "They are indeed AFK" : "They are not AFK");
+            shell.WriteError("Need one argument");
+            return;
         }
 
-        public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+        if (!_players.TryGetSessionByUsername(args[0], out var player))
         {
-            if (args.Length == 1)
-            {
-                return CompletionResult.FromHintOptions(
-                    CompletionHelper.SessionNames(players: _players),
-                    "<playerName>");
-            }
-
-            return CompletionResult.Empty;
+            shell.WriteError("Unable to find that player");
+            return;
         }
+
+        shell.WriteLine(afkManager.IsAfk(player) ? "They are indeed AFK" : "They are not AFK");
+    }
+
+    public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+        {
+            return CompletionResult.FromHintOptions(
+                CompletionHelper.SessionNames(players: _players),
+                "<playerName>");
+        }
+
+        return CompletionResult.Empty;
     }
 }
